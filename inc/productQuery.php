32d8a4686bf1737589a34a01cfccf424d9d605b3
schoon3r <82690423+schoon3r@users.noreply.github.com>
2021-09-29 17:56:02 +0930

    inc add

---
+++ inc/productQuery.php
@@ -0,0 +1,15 @@
+<?php 
+include 'dbhandler.php';
+
+$query = "SELECT products.productName AS NAME, products.description AS DESC, products.price AS PRICE, products.img_1_400x600 AS IMG
+FROM products WHERE products.category_main = 'rebellion'";
+
+$result = $con->query(Squery);
+
+if($result->num_rows != 0){
+    while($rows = $result->fetch_assoc()){
+        $
+    }
+}
+
+?>
